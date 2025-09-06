8fcefd63d359937cb31b0286c74fdb0d7bf20a2a
DanDev1004 <dandeveloper1004@gmail.com>
2025-09-05 22:59:47 -0500

    feat: 🎸 estructura de la db

--- app/Database/scripts/base.sql
+++ app/Database/scripts/base.sql
@@ -1,64 +1,45 @@
-CREATE DATABASE BIBLIOTECA;
-USE BIBLIOTECA;
+CREATE DATABASE biblioteca;
 
--- ubigeo/*
-CREATE TABLE departamentos (
-  id      CHAR(2) PRIMARY KEY,
-  name    VARCHAR(100) NOT NULL
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+USE biblioteca
+-- mantenimiento/*
+CREATE TABLE
+	categorias (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		nombre VARCHAR(70) NOT NULL
+	) ENGINE = INNODB;
 
-CREATE TABLE provincias (
-  id              CHAR(4) PRIMARY KEY,
-  departamento_id CHAR(2) NOT NULL,
-  name            VARCHAR(100) NOT NULL,
-  FOREIGN KEY (departamento_id) REFERENCES departamentos(id)
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE
+	subcategorias (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		nombre VARCHAR(70) NOT NULL,
+		categoria_id INT NOT NULL,
+		FOREIGN KEY (categoria_id) REFERENCES categorias (id)
+	) ENGINE = INNODB;
 
-CREATE TABLE distritos (
-  id           CHAR(6) PRIMARY KEY,
-  provincia_id CHAR(4) NOT NULL,
-  name         VARCHAR(120) NOT NULL,
-  FOREIGN KEY (provincia_id) REFERENCES provincias(id)
-) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
--- */ubigeo
+CREATE TABLE
+	editoriales (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		editorial VARCHAR(70) NOT NULL,
+		nacionalidad VARCHAR(70) NOT NULL
+	) ENGINE = INNODB;
 
-CREATE TABLE libros(
-	id	INT AUTO_INCREMENT PRIMARY KEY,
-	nombre VARCHAR(200) NOT NULL,
-	imagen TEXT NOT NULL
-)ENGINE=INNODB;
+-- */mantenimiento
 
-INSERT INTO libros VALUES 
-	(NULL, 'libro a', 'libroA.jpg'),
-	(NULL, 'libro b', 'libroB.jpg');
-
-SELECT * FROM libros;
-
-
-CREATE TABLE editoriales(
-	id	INT AUTO_INCREMENT PRIMARY KEY,
-	editorial VARCHAR(200) NOT NULL,
-	telefono CHAR(9) NULL,
-	direccion VARCHAR(100) NULL
-)ENGINE=INNODB;
-
-SELECT * FROM editoriales;
-
-
-CREATE TABLE personas(
-	idpersona 	INT AUTO_INCREMENT PRIMARY KEY,
-	dni			CHAR(8) NOT NULL,
-	apellidos	VARCHAR(40) NOT NULL,
-	nombres		VARCHAR(40) NOT NULL,
-	telefono		CHAR(9)	NULL,
-	distrito_id	CHAR(6) NOT NULL,
-	direccion	VARCHAR(100) NULL,
-	FOREIGN KEY (distrito_id) REFERENCES distritos (id)
-)ENGINE=INNODB;
-
-INSERT INTO personas VALUES 
-	(NULL, '75231740', 'Luque', 'Dante', '922836571', '110210', NULL);
-	
-SELECT * FROM personas;
-
-
+CREATE TABLE
+	recursos (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		UUID CHAR(36) NULL,
+		subcategoria_id INT NOT NULL,
+		editorial_id INT NOT NULL,
+		tipo ENUM ('FISICO', 'DIGITAL'),
+		titulo VARCHAR(255),
+		anio_publicacion CHAR(4) NOT NULL,
+		isbn CHAR(13) NOT NULL,
+		num_paginas SMALLINT NOT NULL,
+		ruta_portada TEXT NULL,
+		ruta_recurso TEXT NULL,
+		estado BOOL,
+		created_at DATETIME NULL,
+		updated_at DATETIME NULL,
+		deleted_at DATETIME NULL
+	) ENGINE = INNODB;
